d63c1813d20bf170595fbf015e8c8ddd8fb57695
Renzxs <florencebatol85@gmail.com>
2024-01-11 18:59:06 +0800

    Added Assets and Finishing Weather UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState, useEffect} from 'react';
-import { Wind, Waves} from 'lucide-react';
+import { Wind, Waves, Search} from 'lucide-react';
 import Sun from './assets/sun-icon.png';
 import './index.css';
+import Clear from './assets/clear.png';
+import Cloud from './assets/cloud.png';
+import Drizzle from './assets/drizzle.png';
+import Rain from './assets/rain.png';
+import Snow from './assets/snow.png';
 
 function DateTimeClock() {  
   const [day, setDay] = useState("");
@@ -18,7 +23,7 @@ function DateTimeClock() {
     
     const currentDate = new Date();
     const sec = formatZero(currentDate.getSeconds());
-    const mins = currentDate.getMinutes();
+    const mins = formatZero(currentDate.getMinutes());
     const amOrPm = currentDate.getHours() >= 12 ? "PM" : "AM";
 
     setDate(
@@ -38,18 +43,72 @@ function DateTimeClock() {
   return (
     <div className='overflow-hidden flex justify-start flex-col items-start max-md:items-center max-md:pt-28'>
       <p className='text-white font-montserrat font-extrabold text-[36px] max-lg:text-[20px]'>{day}</p>
-      <h1 className='text-white font-montserrat font-black text-[128px] max-lg:text-[50px]'>{time}</h1>
+      <h1 className='text-white font-montserrat font-black text-[128px] max-lg:text-[40px]'>{time}</h1>
       <p className='text-white font-montserrat font-extrabold text-[36px] max-lg:text-[20px]'>{date}</p>
     </div>
   );
 }
 
+function WeatherChecker() {
+  const [searchInput, setSearchInput] = useState("");
+
+  return (
+    <div className='mt-10 w-[90%] max-sm:w-full p-10 max-sm:p-4 bg-black rounded-[20px] bg-opacity-50'>
+      <form className='flex justify-start items-center gap-4 '>
+        <input value={searchInput} onChange={(e) => {setSearchInput(e.target.value)}} className='font-montserrat w-[50%] max-md:w-[90%] text-white border-2 outline-none border-white bg-transparent rounded-[10px] py-[5px] px-[15px]' placeholder='Search a city' type="text" name="searchInput" id="searchInput" />
+        <button type="submit">
+          <Search className='text-white'></Search>
+        </button>
+      </form>
+
+      <div className='flex justify-around items-center mt-10 max-lg:flex-col gap-10 pb-10'>
+        
+        <div className='flex justify-normal items-center gap-6'>
+          <img className='w-[170px] max-sm:w-[120px]' src={Cloud} alt={Cloud} />
+          <div className='max-md:flex max-md:flex-col-reverse'>
+            <p className='max-md:text-[30px] max-sm:text-[15px]  text-white font-montserrat font-bold text-[30px]'>Republic of the Philippines</p>
+            <h1 className='max-md:text-[50px] max-sm:text-[35px] text-white font-montserrat font-extrabold text-[80px]'>28°C</h1>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-6 justify-start items-start'>
+          <div className='flex justify-start items-center gap-6'>
+              <Wind className='text-white size-[81px] max-md:size-[40px]'></Wind>
+              <div>
+                <h1 className='text-white font-montserrat font-extrabold text-[45px] max-md:text-[25px]'>18 km/h</h1>
+                <p className='text-white  font-montserrat font-medium text-[20px] max-md:text-[15px]'>Wind Speed</p>
+              </div>
+          </div>
+          <div className='flex justify-start items-center gap-6'>
+              <Waves className='text-white size-[81px] max-md:size-[40px]'></Waves>
+              <div>
+                <h1 className='text-white font-montserrat font-extrabold text-[45px] max-md:text-[25px]'>68%</h1>
+                <p className='text-white  font-montserrat font-medium text-[20px] max-md:text-[15px]'>Humidity</p>
+              </div>
+          </div>
+        </div>
+        
+      </div>
+
+
+
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className='w-full text-center max-sm:mt-10 '>
+      <h1 className='font-montserrat text-white font-semibold'>Made by ❤️🔥 by John Florence Batol</h1>
+    </div>
+  );
+}
 
 function App() {
 
   return (
     <>
-    <div className='bg-black'>      
+    <div className='bg-black h-[115vh]'>      
 
       {/* MAIN CONTAINER */}
       <div className='absolute top-0 w-full h-[100vh]'>     
@@ -59,16 +118,22 @@ function App() {
             <img className='w-[40px] transition-all duration-100 ease-linear hover:rotate-45' src={Sun} alt={Sun} />
           </div>
 
-          <div className='ml-10 mt-10 max-md:ml-0 max-md:mt-0'>
+          <div className='ml-16 mt-10 max-md:ml-0 max-md:mt-0'>
             <DateTimeClock />
           </div>
+
+          <div className='p-10 max-sm:p-2 flex justify-center'>
+            <WeatherChecker />
+          </div>
+
+          <Footer />
 
 
         </div>
       </div>   
 
       {/* BACKGROUND GIF */}
-      <div className='bg-black w-full h-[100vh] bg-[url("assets/sunny-bg.gif")] bg-cover bg-no-repeat bg-center opacity-20'>
+      <div className='bg-black w-full h-[100%] bg-[url("assets/sunny-bg.gif")] bg-cover bg-no-repeat bg-center opacity-50'>
 
       </div>
     </div>
---
+++ src/assets/Chills_files/main.jsx
@@ -0,0 +1,18 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=1b23ce32"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=1b23ce32"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=1b23ce32"; const ReactDOM = __vite__cjsImport2_reactDom_client.__esModule ? __vite__cjsImport2_reactDom_client.default : __vite__cjsImport2_reactDom_client;
+import App from "/src/App.jsx?t=1704969155597";
+import "/src/index.css?t=1704969155597";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(React.StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+    fileName: "D:/Developments/ReactJS/weather-app/weather-check/src/main.jsx",
+    lineNumber: 8,
+    columnNumber: 5
+  }, this) }, void 0, false, {
+    fileName: "D:/Developments/ReactJS/weather-app/weather-check/src/main.jsx",
+    lineNumber: 7,
+    columnNumber: 3
+  }, this)
+);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBT0k7QUFQSixPQUFPQSxXQUFXO0FBQ2xCLE9BQU9DLGNBQWM7QUFDckIsT0FBT0MsU0FBUztBQUNoQixPQUFPO0FBRVBELFNBQVNFLFdBQVdDLFNBQVNDLGVBQWUsTUFBTSxDQUFDLEVBQUVDO0FBQUFBLEVBQ25ELHVCQUFDLE1BQU0sWUFBTixFQUNDLGlDQUFDLFNBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQSxTQUFJLEtBRE47QUFBQTtBQUFBO0FBQUE7QUFBQSxTQUVBO0FBQ0YiLCJuYW1lcyI6WyJSZWFjdCIsIlJlYWN0RE9NIiwiQXBwIiwiY3JlYXRlUm9vdCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwic291cmNlcyI6WyJtYWluLmpzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnXG5pbXBvcnQgUmVhY3RET00gZnJvbSAncmVhY3QtZG9tL2NsaWVudCdcbmltcG9ydCBBcHAgZnJvbSAnLi9BcHAuanN4J1xuaW1wb3J0ICcuL2luZGV4LmNzcydcblxuUmVhY3RET00uY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpKS5yZW5kZXIoXG4gIDxSZWFjdC5TdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9SZWFjdC5TdHJpY3RNb2RlPixcbilcbiJdLCJmaWxlIjoiRDovRGV2ZWxvcG1lbnRzL1JlYWN0SlMvd2VhdGhlci1hcHAvd2VhdGhlci1jaGVjay9zcmMvbWFpbi5qc3gifQ==
